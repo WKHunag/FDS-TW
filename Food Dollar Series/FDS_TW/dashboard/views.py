53a1988b3a5eb3fca838e45f80e843241de4aa15
WKHuang <91825822+WKHunag@users.noreply.github.com>
2023-05-25 17:09:00 +0800

    translate english into chinese (#5)

--- Food Dollar Series/FDS_TW/dashboard/views.py
+++ Food Dollar Series/FDS_TW/dashboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Year, IndustryCrossPrimary
+from .models import Year, IndustryCrossPrimary, Marketing, Industry, Primary
 
 # Create your views here.
 def home(request):
@@ -17,10 +17,16 @@ def download(request):
 
 def documentation(request):
     year = Year.objects.order_by('-Year').first()
+    marketing = Marketing.objects.filter(Sector__Name="整體農食", Year=year).first()
+    industry = Industry.objects.filter(Sector__Name="整體農食", Year=year).first()
+    primary = Primary.objects.filter(Sector__Name="整體農食", Year=year).first()
     cross_table = IndustryCrossPrimary.objects.all() 
     # return render(request, "documentation.html", context = {'objects':{'year': year,'nextPeriodData':year.year+5,
     #                                                                    'cross_table': cross_table,}})
     return render(request, "documentation.html", context = {'objects':{'year': year,'nextPeriodData':year.Year+5},
+                                                    'marketing_data': marketing,
+                                                    'industry_data': industry,
+                                                    'primary_data': primary,
                                                     'table_data': cross_table,})
 
 def quickfacts(request):
